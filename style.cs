57071f1836b06d715bf8455eab0190bdb23d838a
Aman bhargava <amanbg001@hmail.comgit config --global user.email>
2024-06-07 22:17:34 +0530

    get itr done aman

---
+++ style.cs
@@ -0,0 +1,27 @@
+  body {
+    color: rgb(102, 73, 20);
+    text-decoration: wavy;
+    text-transform: capitalize;
+    text-align: center;
+    background-image: url("bb.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 100px;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+ .box{
+    height: 100px;
+    width: 100px;
+    border: 32px solid rgb(32, 25, 25) ;
+    margin: 5px;
+    text-align: center;
+    background-color: rgb(143, 176, 103);
+    
+
+}
+
